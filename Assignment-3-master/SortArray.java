a9ecd6d161b52532510d50a26b34f7699ab4ca7c
ManishKumar55 <manish.ldh55@gmail.com>
2018-12-28 00:58:47 +0530

    first commit

---
+++ Assignment-3-master/SortArray.java
@@ -0,0 +1,28 @@
+public class SortArray
+{
+	public static void main(String[] args)
+	{
+	int a[] ={ 1,1,1,1,0,0,1,0}; 
+        int temp=0; 
+        for(int i=0 ; i<a.length;i++){
+	          for (int j=i+1 ; j<a.length; j++)
+	          {
+	              if(a[i]>a[j])
+	              {
+	               temp= a[i];
+	              a[i]=a[j];
+	             a[j]=temp;
+               
+	      }
+	   }
+            
+	}
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]+ " ");
+      }
+
+
+
+	}
+}
